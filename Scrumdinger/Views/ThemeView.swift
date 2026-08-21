fb8f86e8055fd0c86ae5f2780d250f2e00af3a12
Carter <55851465+diency@users.noreply.github.com>
2026-08-20 17:58:57 -0700

    Added basic screens, editing

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Carter Staley on 8/19/26.
+//
+
+import SwiftUI
+import ThemeKit
+
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+            Text(theme.name)
+                .padding(4)
+                .frame(maxWidth: .infinity)
+                .background(theme.mainColor)
+                .foregroundColor(theme.accentColor)
+                .clipShape(RoundedRectangle(cornerRadius: 4))
+        }
+}
+
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
